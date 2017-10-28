77977a83aa6437227cf0fd776bd79a16ac4070c8
aayanqazi <aayanqazi@yahoo.com>
2017-10-28 19:31:58 +0500

    Completed k nearest lab

--- k_nearest_neighbors_lab.py
+++ k_nearest_neighbors_lab.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas
 from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
 my_data = pandas.read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/HSAUR/skulls.csv",delimiter=",")
-
 
 def removeColumns(pandasArray,*columns):
     return pandasArray.drop(pandasArray.columns[[columns]], axis=1).values
@@ -23,15 +23,27 @@ def tagetAndtargetNames(numpyArray, targetColumnIndex):
         target_names.append(targetName)
     return np.asarray(target), target_names
 target , targetNames = tagetAndtargetNames(my_data,1)
+
 X = removeColumns(my_data,0,1)
 y = target
 
-neight = KNeighborsClassifier(n_neighbors=1)
-neight.fit(X,y)
-neight_7 = KNeighborsClassifier(n_neighbors=7)
-neight_7.fit(X,y)
+X_trainset,X_testset,y_trainset,y_testset = train_test_split(X,y,test_size=0.3,random_state=7)
 
-print (neight_7.predict(X[[30]]))
-print (neight.predict(X[[30]]))
+neigh = KNeighborsClassifier(n_neighbors=1)
+neigh23 = KNeighborsClassifier(n_neighbors=23)
+neigh90 = KNeighborsClassifier(n_neighbors=90)
 
-print (my_data)
+neigh.fit(X_trainset,y_trainset)
+neigh23.fit(X_trainset,y_trainset)
+neigh90.fit(X_trainset,y_trainset)
+
+pred = neigh.predict(X_testset)
+pred23 = neigh23.predict(X_testset)
+pred90 = neigh90.predict(X_testset)
+
+print ("Neigh Accuracy",metrics.accuracy_score(y_testset,pred))
+print ("Neight23 Accuracy", metrics.accuracy_score(y_testset,pred23))
+print ("Neight90 Accuracy", metrics.accuracy_score(y_testset,pred90))
+
+
+
